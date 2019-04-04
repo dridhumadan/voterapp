754b3a216fe3d2e01bbb1b8ac39b2af843579f08
Dridhu Madan <dridhumadan@gmail.com>
2019-04-04 19:15:48 +0530

    CSS Changes

--- voterapp/static/js/dashboard.js
+++ voterapp/static/js/dashboard.js
@@ -1,9 +1,3 @@
-logout = document.getElementById('logout');
-
-logout.onclick = function() {
-    window.location.href = '/auth/logout';
-};
-
 vs = document.getElementsByClassName('vs')[0];
 span = document.getElementsByClassName('status')[0];
 fontawesome = document.getElementById('vs');
