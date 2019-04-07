b41891f950c2abf3510000bc5091c54a83ddb94d
Dridhu Madan <dridhumadan@gmail.com>
2019-04-08 01:33:52 +0530

    Changes to voting procedure

--- voterapp/users/routes.py
+++ voterapp/users/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, flash
 from voterapp.models import Voter, Candidate, Constituency
 from voterapp import db
 from flask_login import current_user, login_required
@@ -9,18 +9,30 @@ users = Blueprint('users', __name__)
 @users.route('/vote', methods = ['GET', 'POST'])
 @login_required
 def vote():
+    cid = Voter.query.filter_by(voter_id = current_user.voter_id).first().cand_id
+    if cid != None:
+        return render_template('users/vote_complete.html', title = 'Vote')
     candidates = Candidate.query.filter_by(const_code = current_user.const_code).all()
     con = Constituency.query.filter_by(const_code = current_user.const_code).first().const_name
-    return render_template('users/vote.html', title = 'Vote', cand = candidates, con = con)
+    return render_template('users/vote.html', title = 'Vote', cand = candidates, con = con, login_status = 2)
 
 
 @users.route('/vote/<string:cand_id>', methods=['GET', 'POST'])
+@login_required
 def cast_vote(cand_id):
     voter = Voter.query.filter_by(voter_id = current_user.voter_id).update(dict(cand_id = cand_id))
     db.session.commit()
+    flash('Voting Successful!')
     return redirect(url_for('home.dashboard'))
 
 
 @users.route('/candidates')
+@login_required
 def candidates():
     return render_template('users/candidates.html', title = 'Candidates')
+
+
+@users.route('/account')
+@login_required
+def account():
+    return render_template('users/account.html', title = 'Account')
